11bdc25d3268dbffde1a7734f8f886f63de2d73b
raguzman <55261233+rafaguzmang@users.noreply.github.com>
2024-06-05 17:29:51 -0600

    Se agrega OT al modulo de ventas (dtm_ordenes_compra)

--- __manifest__.py
+++ __manifest__.py
@@ -4,12 +4,13 @@
     'version': '1.0',
     'author': "Rafael Guzmán",
     "description": "Visualización de las ordenes de compra",
-    "depends":["dtm_cotizaciones"],
+    "depends":["dtm_cotizaciones","dtm_procesos","dtm_odt"],
     "data":[
         'security/ir.model.access.csv',
         #Views
         'views/dtm_ordenes_compra_views.xml',
-        'views/dtm_ordenes_compra_facturado_views.xml'
+        'views/dtm_ordenes_compra_facturado_views.xml',
+        'views/dtm_ventas_ot_view.xml'
     ]
 }
 
--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,3 @@
 from . import dtm_ordenes_compra
 from . import dtm_ordenes_compra_facturado
+from . import dtm_ventas_ot
